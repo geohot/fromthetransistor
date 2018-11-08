1f2e7a2cd1a68a0aef88ab22bf218ead912ee31d
George Hotz <geohot@gmail.com>
2018-11-08 23:45:08 +0900

    refactor spi into a class

--- section2/adept.py
+++ section2/adept.py
@@ -4,14 +4,6 @@ import usb1
 import time
 from hexdump import hexdump
 
-context = usb1.USBContext()
-handle = context.openByVendorIDAndProductID(0x1443, 0x0007)
-#handle.setConfiguration(0)
-handle.claimInterface(0)
-dev = handle.getDevice()
-print(dev)
-#print(dir(dev))
-
 # DspiEnableEx:   3 6 0 <1: port request>
 # DspiDisable:    3 6 1 <1: port request>
 # DspiSetSpeed:   7 6 3 <1: active port> <4: speed>
@@ -23,63 +15,57 @@ print(dev)
 #                       <1: should receive?> <4: number of bytes>
 # (fSelStart, fSelEnd, rgbSnd, rgbRcv, cbSend, fOverlap)
 
-def spi_get_speed():
-  handle.bulkWrite(1, struct.pack("BBBB", 3, 6, 4, 0))
-  ret = handle.bulkRead(2, 0x10)
-  assert len(ret) == 6
-  assert ret[0] == 5
-  assert ret[1] == 0
-  return struct.unpack("I", ret[2:])[0]
+class SPI(object):
+  CMD_ENABLE = 0
+  CMD_DISABLE = 1
+  CMD_SET_SPEED = 3
+  CMD_GET_SPEED = 4
+  CMD_SET_SPI_MODE = 5
+  CMD_SET_SELECT = 6
+  CMD_PUT = 7
 
-def spi_put(b):
-  fSelStart = 0
-  fSelEnd = 1
-  rcv = 1
-  handle.bulkWrite(1, struct.pack("BBBBBBBI", 10, 6, 7, 0, fSelStart, fSelEnd, rcv, len(b)))
-  handle.bulkWrite(3, b)
-  print(handle.bulkRead(2, 0x10))
-  hexdump(handle.bulkRead(2, 0x10))
-  hexdump(handle.bulkRead(4, 0x40))
-  #time.sleep(1.0)
+  def __init__(self, port):
+    self.context = usb1.USBContext()
+    self.handle = self.context.openByVendorIDAndProductID(0x1443, 0x0007)
+    self.handle.claimInterface(0)
 
-def spi_enable(port):
-  handle.bulkWrite(1, struct.pack("BBBB", 3, 6, 0, port))
-  print(handle.bulkRead(2, 0x10))
+    self.port = port
+    self.enable()
 
-def spi_disable(port):
-  handle.bulkWrite(1, struct.pack("BBBB", 3, 6, 1, port))
-  print(handle.bulkRead(2, 0x10))
+  def __del__(self):
+    self.disable()
+    self.handle.close()
 
-spi_enable(0)
-print(spi_get_speed())
+  def send_cmd_simple(self, num):
+    self.handle.bulkWrite(1, struct.pack("BBBB", 3, 6, num, self.port))
+    ret = self.handle.bulkRead(2, 0x10)
+    hexdump(ret)
+    return ret
 
-spi_put(b"\x9e" + b"\x00"*20)
+  def enable(self):
+    ret = self.send_cmd_simple(self.CMD_ENABLE)
 
-print("disable")
-spi_disable(0)
+  def get_speed(self):
+    ret = self.send_cmd_simple(self.CMD_GET_SPEED)
+    assert len(ret) == 6
+    assert ret[0] == 5
+    assert ret[1] == 0
+    return struct.unpack("I", ret[2:])[0]
 
-exit(0)
+  def disable(self):
+    ret = self.send_cmd_simple(self.CMD_DISABLE)
+    assert ret == b"\x01\x00"
 
+  def put(self, b, fSelStart=0, fSelEnd=1):
+    rcv = 1
+    self.handle.bulkWrite(1, struct.pack("BBBBBBBI", 10, 6, 7, 0, fSelStart, fSelEnd, rcv, len(b)))
+    self.handle.bulkWrite(3, b)
+    hexdump(self.handle.bulkRead(2, 0x10))
+    hexdump(self.handle.bulkRead(2, 0x10))
+    hexdump(self.handle.bulkRead(4, 0x40))
 
-for i in range(8):
-  try:
-    mps = dev.getMaxPacketSize(i)
-    print(i, mps)
-  except Exception:
-    pass
-
-# enable
-#handle.bulkWrite(1, struct.pack("BBBB", 3, 6, 4, 0))
-#print(handle.bulkRead(2, 0x10))
-
-print(spi_get_speed())
-
-handle.bulkWrite(1, struct.pack("BBBB", 3, 6, 1, 0))
-print(handle.bulkRead(2, 0x10))
-
-
-handle.close()
-
-#print(handle.bulkRead(1, 0x10))
-
+spi = SPI(0)
+print(spi.get_speed())
+spi.put(b"\x9e" + b"\x00"*20)
+del spi
 

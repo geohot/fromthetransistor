9ef169c4828d432db099cea5a76c24a4af4f526c
George Hotz <geohot@gmail.com>
2018-11-21 22:37:40 +0800

    ay, you got a license for that map

--- section2/blink/build.sh
+++ section2/blink/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-mkdir -p out
-
 # synthesize the verilog
-xilinx xst -ifn go.scr
+#xilinx xst -ifn go.scr
 
+# ngdbuild? turns netlist into Native Generic Database?
+#xilinx ngdbuild -p spartan6 go.ngc go.ngd
+
+# map
+xilinx map -p xc6slx9-csg324 -w go.ngd -o go.map.ncd go.pcf
 

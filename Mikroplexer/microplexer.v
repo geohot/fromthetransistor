de9a7ad873d37ad4d9ec84fe7215dccc5b94c53a
Kent Åsvang <kentaasvang@gmail.com>
2021-08-06 21:46:36 +0200

    simple microplexer

--- Mikroplexer/microplexer.v
+++ Mikroplexer/microplexer.v
@@ -1,9 +1,18 @@
+// this is a simple implementation of a microplexer.
+// the difference between a microplexer and a LUT is that 
+// a lut has programmable memory for the inputs
+// that way they can manually decide where the current goes
+// so that they can implement advanced hardware digitally
 
 module microplexer(
-  input   reg[5:0] set
-  output
+  output wire wire_out,
+  input wire[3:0] sel4,
+  input wire[3:0] inputs
 );
-
-initial begin
   
-end
+  assign wire_out = (inputs[3] & sel4[3]) || 
+                    (inputs[2] & sel4[2]) || 
+                    (inputs[1] & sel4[1]) || 
+                    (inputs[0] & sel4[0]);
+
+endmodule
--- Mikroplexer/microplexer_tb.v
+++ Mikroplexer/microplexer_tb.v
@@ -1,11 +1,30 @@
+`timescale 1s / 1s
+`include "microplexer.v"
 
-module microplexer_tb(
-  output reg[5:0] set
-);
-
-  initial begin
-    set = 6'b000010;
-    $display("%b", set);
+module microplexer_tb();
+  
+	output wire[3:0] inputs;
+  output wire[3:0] sel4;
+  input wire wire_in;
+	
+	// need register to hold values
+	reg[3:0] reg_inputs;
+  
+	initial begin
+    $dumpfile("microplexer.vcd");
+    $dumpvars(0, microplexer_tb);
+	
+		reg_inputs = 4'b1000;			// expected wire_in = 0
+		#2 reg_inputs = 4'b0100;	// expected wire_in = 0
+		#2 reg_inputs = 4'b0010;	// expected wire_in = 0
+		#2 reg_inputs = 4'b0001; 	// expected wire_in = 1
+    #10 $finish;
   end
 
+	assign inputs = reg_inputs;
+	assign sel4 = 4'b0001; // one-hot encoding
+
+	// plug device	
+	microplexer MyMicro(wire_in, sel4, inputs);
+  
 endmodule
